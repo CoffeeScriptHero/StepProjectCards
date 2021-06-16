f51f6036fb07dc029ebb17366ccfd252f4fa9f59
Larysa Kutyrkina <kutyrkina.larysa@gmail.com>
2021-06-16 14:51:26 +0300

    some changes

--- step project cards/login/modalLogin.js
+++ step project cards/login/modalLogin.js
@@ -1,32 +1,17 @@
-export default class ModalLogin {
+export class modalLogin {
    constructor() {
-      this.modal = this.create()
+      this.modal = create();
    }
-   create () {
-      const modalWrapper = document.createElement("div");
-      modalWrapper.classList.add("modal-wrapper");
+      create() {
+         const loginBtn = document.querySelector("login-btn");
+         const loginModal = document.querySelector(".modal");
 
-      modalWrapper.innerHTML = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Войти в аккаунт</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body"></div>
-            </div>
-          </div>
-        </div>
-        `;
-
-      return modalWrapper.firstChild;
-   }
-
-   render(content) {
-      const modalBody = this.modal.querySelector(".modal-body");
-      modalBody.append(content);
-
-      return this.modal;
-   }
+         loginBtn.addEventListener("click", async (e) => {
+            e.preventDefault();
+            loginModal.style.display = "";
+         });
+      }
 
 }
+
+
